f4e7612b138b9920e342f040e8921bd9040548d3
Lindsey Renken <lmrenken@gmail.com>
2020-03-27 12:54:33 -0500

    Adds source links

--- curve.py
+++ curve.py
@@ -1,3 +1,18 @@
+# https://datahub.io/core/covid-19
+# https://github.com/CSSEGISandData/COVID-19/
+# https://www.reddit.com/r/datasets/comments/exnzrd/coronavirus_datasets/
+# http://virological.org/t/phylodynamic-analysis-176-genomes-6-mar-2020/356
+# https://github.com/nextstrain/ncov
+# https://github.com/nytimes/covid-19-data
+# https://www.nejm.org/doi/full/10.1056/NEJMoa2001316
+# https://coronavirus-resources.esri.com/app/ba772aff9efc4374a3a64457b9240300
+# https://nssac.bii.virginia.edu/covid-19/dashboard/
+# https://coronavirus-resources.esri.com/
+# https://txdshs.maps.arcgis.com/apps/opsdashboard/index.html#/ed483ecd702b4298ab01e8b9cafc8b83
+# https://covidtracking.com/
+# https://covidtracking.com/api/
+# https://ourworldindata.org/covid-testing#covid-19-test-coverage-estimates-as-of-20-march
+
 import pandas as pd
 import math
 import plotly.graph_objects as go
