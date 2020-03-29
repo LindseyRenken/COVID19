2e15b07bf06fb3cda5d701ba0d242f12493d9086
Lindsey Renken <lmrenken@gmail.com>
2020-03-29 14:39:26 -0500

    Filter ans sort last day df

--- codivtracking-curve.py
+++ codivtracking-curve.py
@@ -1,3 +1,12 @@
+# Objective: Determine who is flattening their curve.
+# 3-day rolling average of new cases
+# extrapolate next point
+# plot number of hospital beds per region
+# Calculate these curves starting with day 1 as when they hit 100 cases.
+
+# Alert on emerging outbreaks in regions
+# Show who's testing the most
+
 import pandas as pd
 import io
 import os
@@ -20,31 +29,15 @@ def calculatePercentage(p, t):
 df['date'] = list(
     map(lambda s: dateutil.parser.parse(s).date(), df['dateChecked']))
 df['percent_positive'] = list(
-    map(calculatePercentage, df['positive'], df['tested']))
+    map(calculatePercentage, df['positive'], df['totalTestResults']))
 
-df = df.loc[df['date'] > datetime.date(2020, 3, 14)]
+df = df.loc[df['date'] > datetime.date(2020, 3, 10)]
+
 
 groups = list(df.groupby('state'))
 
 if not os.path.exists("images"):
     os.mkdir("images")
-
-last = list(df.groupby('date'))[-1]
-last_date = last[0]
-last_df = last[1]
-states = list(last_df['state'])
-
-fig = go.Figure(data=[
-    go.Bar(name='Deceased', x=states, y=list(last_df['death'])),
-    # go.Bar(name='Hospitalized', x=states, y=list(last_df['hospitalized'])),
-    # go.Bar(name='Positive', x=states, y=list(last_df['positive'])),
-    # go.Bar(name='Negative', x=states, y=list(last_df['negative'])),
-    # go.Bar(name='Pending', x=states, y=list(last_df['pending']))
-])
-
-fig.update_layout(barmode='stack')
-fig.show()
-
 
 # Shows cummulative positive tests per day
 fig = go.Figure()
@@ -64,7 +57,6 @@ fig.update_layout(
 )
 
 fig.show()
-
 
 # Shows cummulative tests per day
 fig = go.Figure()
@@ -102,4 +94,23 @@ fig.update_layout(
     )
 )
 
+fig.show()
+
+# Bar graph of cummulative pos,neg,tested,deaths,pending
+last = list(df.groupby('date'))[-1]
+last_date = last[0]
+last_df = last[1]
+last_df = last_df.loc[last_df['positive'] > 200]
+last_df = last_df.sort_values(['positive'], ascending=False)
+states = list(last_df['state'])
+
+fig = go.Figure(data=[
+    go.Bar(name='Deceased', x=states, y=list(last_df['death'])),
+    go.Bar(name='Hospitalized', x=states, y=list(last_df['hospitalized'])),
+    go.Bar(name='Positive', x=states, y=list(last_df['positive'])),
+    go.Bar(name='Negative', x=states, y=list(last_df['negative'])),
+    # go.Bar(name='Pending', x=states, y=list(last_df['pending']))
+])
+
+fig.update_layout(barmode='stack')
 fig.show()
