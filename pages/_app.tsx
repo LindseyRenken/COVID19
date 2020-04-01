df9e52f82e2e79324ce97583ba62a2e12560ba90
Lindsey Renken <lmrenken@gmail.com>
2020-03-31 23:34:49 -0500

    Sets up styles for app

---
+++ pages/_app.tsx
@@ -0,0 +1,37 @@
+import "./styles.scss";
+import App from "next/app";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { Breakpoints } from "react-device-breakpoints";
+import { ThemeProvider as MaterialThemeProvider } from "@rmwc/theme";
+
+const theme = {
+  colors: {
+    primary: "white"
+  }
+};
+
+const breakpoints = {
+  isDesktop: "(min-width: 1024px)",
+  isTablet: "(max-width: 1023px) and (min-width: 768px)",
+  isMobile: "(max-width: 767px)"
+};
+
+const materialTheme: Record<string, string> = {
+  topAppBarFillColor: "FFFFFF"
+};
+
+export default class CovidCurveApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <MaterialThemeProvider options={materialTheme}>
+        <ThemeProvider theme={theme}>
+          <Breakpoints {...breakpoints}>
+            <Component {...pageProps} />
+          </Breakpoints>
+        </ThemeProvider>
+      </MaterialThemeProvider>
+    );
+  }
+}
