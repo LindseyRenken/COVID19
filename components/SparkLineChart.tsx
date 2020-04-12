b1f449653c04c2e47f43b9964f024e15f6bd6750
Lindsey Renken <lmrenken@gmail.com>
2020-04-12 15:21:45 -0500

    Only renders pie chart in view

--- components/SparkLineChart.tsx
+++ components/SparkLineChart.tsx
@@ -1,10 +1,6 @@
-// import * as am4core from "@amcharts/amcharts4/core";
-// import * as am4charts from "@amcharts/amcharts4/charts";
-// import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import { Component } from "react";
 import { Chart, Util } from "@antv/g2";
-
-// am4core.useTheme(am4themes_animated);
+import { InView } from "react-intersection-observer";
 
 interface Props {
   positive: number;
@@ -13,53 +9,69 @@ interface Props {
   id: string;
 }
 
-class SparkLineChart extends Component<Props, {}> {
-  componentDidMount() {
-    const chart = new Chart({
-      container: "chartdiv_" + this.props.id,
-      autoFit: true,
-      height: 50,
-    });
-    // Add data
-    const data = [
-      {
-        category: "Positive",
-        value: this.props.positive,
-      },
-      {
-        category: "Negative",
-        value: this.props.negative,
-      },
-      {
-        category: "Pending",
-        value: this.props.pending,
-      },
-    ];
-    chart.data(data);
+interface State {
+  chartSet: boolean;
+}
 
-    chart.coordinate("theta", {
-      radius: 0.75,
-    });
-    chart.tooltip(false);
+class SparkLineChart extends Component<Props, State> {
+  state: State = {
+    chartSet: false,
+  };
 
-    chart
-      .interval()
-      .adjust("stack")
-      .position("value")
-      .color("category", ["#86BBD8", "#F26419", "#758E4F"])
-      .label(false);
+  setActive(inView: boolean) {
+    if (!inView) return;
+    if (this.state.chartSet) return;
+    else {
+      const chart = new Chart({
+        container: "chartdiv_" + this.props.id,
+        autoFit: true,
+        height: 50,
+      });
+      // Add data
+      const data = [
+        {
+          category: "Positive",
+          value: this.props.positive,
+        },
+        {
+          category: "Negative",
+          value: this.props.negative,
+        },
+        {
+          category: "Pending",
+          value: this.props.pending,
+        },
+      ];
+      chart.data(data);
 
-    chart.legend(false);
+      chart.coordinate("theta", {
+        radius: 0.75,
+      });
+      chart.tooltip(false);
 
-    chart.render();
+      chart
+        .interval()
+        .adjust("stack")
+        .position("value")
+        .color("category", ["#86BBD8", "#F26419", "#758E4F"])
+        .label(false);
+
+      chart.legend(false);
+
+      chart.render();
+
+      this.setState({ chartSet: true });
+    }
   }
 
   render() {
     return (
-      <div
-        id={"chartdiv_" + this.props.id}
-        style={{ height: "75px", width: "75px" }}
-      ></div>
+      <InView as="div" onChange={(inView, entry) => this.setActive(inView)}>
+        <div
+          id={"chartdiv_" + this.props.id}
+          style={{ height: "75px", width: "75px" }}
+        ></div>
+      </InView>
     );
   }
 }
